2507714d7e046799ffc7ff5ee771baad86e04dd9
melnik <melnik@bifit.com>
2021-11-01 12:58:50 +0300

    first commit

---
+++ src/components/Notes/Form.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="note-form__wrapper">
+    <form class="note-form" @submit.prevent="onSubmit">
+      <textarea required v-model="value" placeholder="Type ur note" />
+      <TagsList
+        @onItemClick="handleTagClick"
+        :items="tags"
+        :activeItems="activeTags"
+      />
+      <button class="btn btnPrimary" type="submit">Add new note</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import TagsList from "@/components/UI/TagsList.vue";
+export default {
+  components: { TagsList },
+  data() {
+    return {
+      value: "",
+      tags: ["home", "work", "travel"],
+      activeTags: [],
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$emit("onSubmit", this.value, this.activeTags);
+      this.value = "";
+      this.activeTags = [];
+    },
+    handleTagClick(tag) {
+      if (!this.activeTags.includes(tag)) {
+        this.activeTags.push(tag);
+      } else {
+        this.activeTags = this.activeTags.filter((item) => item !== tag);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.note-form__wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.note-form {
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  width: 100%;
+  textarea {
+    margin-bottom: 0;
+  }
+}
+</style>
